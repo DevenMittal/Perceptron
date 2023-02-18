ad1b9519a87c1a493933655a1c9ba5111c30049b
devenmittal@gmail.com <devenmittal@gmail.com>
2023-02-18 10:33:20 -0800

    Add project files.

---
+++ Perceptron/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Perceptron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] weights = { .75, -1.25 };
+            Perceptron perceptron = new Perceptron(weights, .5);
+
+            double[][] inputs =
+            {
+                new double[] {0, 0},
+                new double[] {.3, -.7},
+                new double[] {1, 1},
+                new double[] {-1, -1},
+                new double[] {-.5, .5}
+
+            };
+
+            double[] values = perceptron.Compute(inputs);
+
+            ;
+
+
+
+
+
+        }
+    }
+}
